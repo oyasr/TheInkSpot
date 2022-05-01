916ebdbaa13b286111f2d09462848506a36a9093
MahmoudSaeed13 <mahmoud.said2019@gmail.com>
2022-05-01 17:49:45 +0200

    created user model and its test cases

--- theinkspot/users/models.py
+++ theinkspot/users/models.py
@@ -1,26 +1,51 @@
-from django.contrib.auth.models import AbstractUser
-from django.db.models import CharField
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
+from django.db import models
 from django.urls import reverse
+from django.utils.timezone import now
 from django.utils.translation import gettext_lazy as _
 
 
-class User(AbstractUser):
-    """
-    Default custom user model for TheInkSpot.
-    If adding fields that need to be filled at user signup,
-    check forms.SignupForm and forms.SocialSignupForms accordingly.
-    """
+class UserManager(BaseUserManager):
 
-    #: First and last name do not cover name patterns around the globe
-    name = CharField(_("Name of User"), blank=True, max_length=255)
-    first_name = None  # type: ignore
-    last_name = None  # type: ignore
+    def create_user(self, name, username, email, password=None):
+        if username is None:
+            raise TypeError('user must have username')
+        if name is None:
+            raise TypeError('user must have name')
+        if email is None:
+            raise TypeError('user must have email')
+        user = self.model(username=username,
+                          name=name,
+                          email=self.normalize_email(email))
+        user.set_password(password)
+        user.save()
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    name = models.CharField(_('User Full Name'), max_length=155)
+    email = models.EmailField(_('Email'),max_length=155, unique=True)
+    username = models.CharField(_('Username'), max_length=155, unique=True)
+    is_verified = models.BooleanField(_('Is user verified by email'), default=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(default=now)
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['name', 'email']
+
+    objects = UserManager()
 
     def get_absolute_url(self):
         """Get url for user's detail view.
-
         Returns:
             str: URL for user detail.
-
         """
-        return reverse("users:detail", kwargs={"username": self.username})
+        return reverse("users:allusers", kwargs={"username": self.username})
+
+    def __str__(self):
+        return self.username
