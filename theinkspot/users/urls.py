eeb76bfdbfd41ef675ed6d3b4df030b2e394877d
shroukhegazi <shroukhegazi99@gmail.com>
2022-05-03 19:29:49 +0200

    modified urls

--- theinkspot/users/urls.py
+++ theinkspot/users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from theinkspot.users.views import user_detail_view, user_redirect_view, user_update_view, UserViewSet
+from theinkspot.users.views import user_detail_view, user_redirect_view, user_update_view
 
 app_name = "users"
 urlpatterns = [
