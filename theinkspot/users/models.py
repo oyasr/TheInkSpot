a5927ce2f7f95a901b4b830f94206f5246719370
MahmoudSaeed13 <mahmoud.said2019@gmail.com>
2022-05-03 14:20:00 +0200

    fixed linter and fixed import error in pre-commit black

--- theinkspot/users/models.py
+++ theinkspot/users/models.py
@@ -10,33 +10,32 @@ from django.utils.translation import gettext_lazy as _
 
 
 class UserManager(BaseUserManager):
-
     def create_user(self, name, username, email, password=None):
         if username is None:
-            raise TypeError('user must have username')
+            raise TypeError("user must have username")
         if name is None:
-            raise TypeError('user must have name')
+            raise TypeError("user must have name")
         if email is None:
-            raise TypeError('user must have email')
-        user = self.model(username=username,
-                          name=name,
-                          email=self.normalize_email(email))
+            raise TypeError("user must have email")
+        user = self.model(
+            username=username, name=name, email=self.normalize_email(email)
+        )
         user.set_password(password)
         user.save()
         return user
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-    name = models.CharField(_('User Full Name'), max_length=155)
-    email = models.EmailField(_('Email'),max_length=155, unique=True)
-    username = models.CharField(_('Username'), max_length=155, unique=True)
-    is_verified = models.BooleanField(_('Is user verified by email'), default=False)
+    name = models.CharField(_("User Full Name"), max_length=155)
+    email = models.EmailField(_("Email"), max_length=155, unique=True)
+    username = models.CharField(_("Username"), max_length=155, unique=True)
+    is_verified = models.BooleanField(_("Is user verified by email"), default=False)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     date_joined = models.DateTimeField(default=now)
 
-    USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = ['name', 'email']
+    USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = ["name", "email"]
 
     objects = UserManager()
 
--- theinkspot/users/tests/test_models.py
+++ theinkspot/users/tests/test_models.py
@@ -5,68 +5,67 @@ from theinkspot.users.models import User
 pytestmark = pytest.mark.django_db
 
 
-
-
 @pytest.fixture
 def user(db) -> User:
-    return User.objects.create_user(name='user name', 
-                                    username='username',
-                                    email='test@email.com', 
-                                    password='Am0123456789123456')
+    return User.objects.create_user(
+        name="user name",
+        username="username",
+        email="test@email.com",
+        password="Am0123456789123456",
+    )
+
 
 @pytest.mark.django_db
 class TestUserModel:
-    
     def test_update_user(self, user):
-        user.name = 'mahmoud saeed ali hussain'
+        user.name = "mahmoud saeed ali hussain"
         user.save()
-        user = User.objects.get(name='mahmoud saeed ali hussain')
-        assert user.name == 'mahmoud saeed ali hussain'
+        user = User.objects.get(name="mahmoud saeed ali hussain")
+        assert user.name == "mahmoud saeed ali hussain"
 
     def test_filter_user(self, user):
-        assert User.objects.filter(username='username').exists()
+        assert User.objects.filter(username="username").exists()
 
     def test_object_name_is_first_name(self, user):
-        object_name = f'{user.username}'
+        object_name = f"{user.username}"
         assert str(user) == object_name
 
     def test_object_name_label(self, user):
-        name_label = user._meta.get_field('name').verbose_name
-        assert name_label == 'User Full Name'
+        name_label = user._meta.get_field("name").verbose_name
+        assert name_label == "User Full Name"
 
     def test_object_email_label(self, user):
-        email_label = user._meta.get_field('email').verbose_name
-        assert email_label == 'Email'
+        email_label = user._meta.get_field("email").verbose_name
+        assert email_label == "Email"
 
     def test_object_username_label(self, user):
-        username_label = user._meta.get_field('username').verbose_name
-        assert username_label == 'Username'
+        username_label = user._meta.get_field("username").verbose_name
+        assert username_label == "Username"
 
     def test_object_is_is_verified_label(self, user):
-        is_verified_label = user._meta.get_field('is_verified').verbose_name
-        assert is_verified_label == 'Is user verified by email'
+        is_verified_label = user._meta.get_field("is_verified").verbose_name
+        assert is_verified_label == "Is user verified by email"
 
     def test_name_max_length(self, user):
-        max_length = user._meta.get_field('name').max_length
+        max_length = user._meta.get_field("name").max_length
         assert max_length == 155
 
     def test_username_max_length(self, user):
-        max_length = user._meta.get_field('username').max_length
+        max_length = user._meta.get_field("username").max_length
         assert max_length == 155
 
-    def test_username_max_length(self, user):
-        max_length = user._meta.get_field('email').max_length
+    def test_email_max_length(self, user):
+        max_length = user._meta.get_field("email").max_length
         assert max_length == 155
 
     def test_email_is_unique(self, user):
-        is_email_unique = user._meta.get_field('email').unique
-        assert is_email_unique == True
-        
+        is_email_unique = user._meta.get_field("email").unique
+        assert is_email_unique is True
+
     def test_username_is_unique(self, user):
-        is_username_unique = user._meta.get_field('username').unique
-        assert is_username_unique == True
+        is_username_unique = user._meta.get_field("username").unique
+        assert is_username_unique is True
 
     def test_is_verified_is_fasle_by_default(self, user):
-        is_verified_defaulted_false = user._meta.get_field('is_verified').default
-        assert is_verified_defaulted_false == False
-
+        is_verified_defaulted_false = user._meta.get_field("is_verified").default
+        assert is_verified_defaulted_false is False
