220a732b599f5ac655b2d4fcfe8daedb76c57c2a
shroukhegazi <shroukhegazi99@gmail.com>
2022-05-11 10:23:47 +0200

    Added token urls and modified requirments

--- config/api_router.py
+++ config/api_router.py
@@ -3,6 +3,7 @@ from django.urls import path
 from rest_framework.routers import DefaultRouter, SimpleRouter
 
 from theinkspot.users.api.views import RegisterUsers, UserViewSet
+from rest_framework_simplejwt import views as jwt_views
 
 if settings.DEBUG:
     router = DefaultRouter()
@@ -18,4 +19,6 @@ urlpatterns = router.urls
 urlpatterns += [
     path("users/allusers/", UserViewSet.as_view({"get": "list"}), name="allusers"),
     path("users/register/", RegisterUsers.as_view(), name="register"),
+    path('token/', jwt_views.TokenObtainPairView.as_view(),name='access token'),
+    path('refresh/token/', jwt_views.TokenRefreshView.as_view(),name='refresh token'),
 ]
